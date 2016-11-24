ddf71e493f74ec8253ed7940ea1953ff5c167fd5
moiseencoo <95varya@mail.ru>
2016-11-24 17:17:12 +0500

    Initial stage

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+
+$(window).scroll(function(){
+    
+if ($(window).scrollTop() > 180 && $(window).width() > 768)
+	{
+		 $('.navbar').addClass('show');
+	}
+	else
+	{
+		$('.navbar').removeClass('show');
+       
+	}
+});
+
+
+
+$('.search_icon').click(function(){
+    $('.search_form').toggle();
+    $('.search_icon').toggle();
+});
+
+$('.m_menu').click(function(){
+    $('.mobile-navigation').toggle('slow');
+});
