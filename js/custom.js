afeec2442044b28f2d8a34f13f408ee0879f41de
moiseencoo <95varya@mail.ru>
2016-12-05 19:06:30 +0500

    Trying not to die

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,32 @@
-
-$(window).scroll(function(){
-    
-if ($(window).scrollTop() > 180 && $(window).width() > 768)
-	{
-		 $('.navbar').addClass('show');
+$(window).scroll(function () {
+	if ($(window).scrollTop() > 180 && $(window).width() > 768) {
+		$('.navbar').addClass('navbar-fixed-top');
+		$('#navbar').removeClass('normal-menu');
+		$('#navbar').addClass('fixed-menu');
+		$('.navbar-header').show();
 	}
-	else
-	{
-		$('.navbar').removeClass('show');
-       
+	else {
+		$('.navbar').removeClass('navbar-fixed-top');
+		$('#navbar').removeClass('fixed-menu');
+		$('#navbar').addClass('normal-menu');
+		$('.navbar-header').hide();
 	}
 });
-
-
-
-$('.search_icon').click(function(){
-    $('.search_form').toggle();
-    $('.search_icon').toggle();
+$('.search_icon').click(function () {
+	$('.search_form').toggle();
+	$('.search_icon').toggle();
 });
-
-$('.m_menu').click(function(){
-    $('.navbar').toggle('slow');
+$('.m_menu').click(function () {
+	$('.navbar').toggle('slow');
 });
+/*$('.dropdown').click(function () {
+	if ($(this).hasClass('open')) {
+		$(this).find('.dropdown-menu').hide();
+	}
+	else {
+		$(this).find('.dropdown-menu').show();
+	}
+});
+$('.dropdown-menu li').click(function () {
+	$(this).find('.submenu').toggle();
+});*/
