a741a164f065549d85265fa2cf75c1fd2727ea4d
moiseencoo <95varya@mail.ru>
2016-12-07 18:56:09 +0500

    Last Mebel modifications before sending it to Tatyana

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,65 @@
+$(document).ready(function () {
+	if ($(window).width() < 769) {
+		$('.nav .dropdown-toggle').attr('data-toggle', 'none');
+		$('.dropdown-toggle').click(function () {
+			if ($(this).parent('#dropdown').hasClass('open')) {
+				$(this).parent('#dropdown').removeClass('open');
+			}
+			else {
+				$(this).parent('#dropdown').addClass('open');
+			}
+		});
+	}
+	else {
+		$('.nav .dropdown-toggle').attr('data-toggle', 'dropdown');
+	}
+});
 $(window).scroll(function () {
-	if ($(window).scrollTop() > 180 && $(window).width() > 768) {
+	if ($(window).scrollTop() > 180 && $(window).width() > 992) {
 		$('.navbar').addClass('navbar-fixed-top');
 		$('#navbar').removeClass('normal-menu');
 		$('#navbar').addClass('fixed-menu');
 		$('.navbar-header').show();
+		$('.search_form input').removeClass('onTop');
 	}
 	else {
 		$('.navbar').removeClass('navbar-fixed-top');
 		$('#navbar').removeClass('fixed-menu');
 		$('#navbar').addClass('normal-menu');
 		$('.navbar-header').hide();
+		$('.search_form input').addClass('onTop');
 	}
 });
 $('.search_icon').click(function () {
-	$('.search_form').toggle();
-	$('.search_icon').toggle();
+	if ($(window).width() < 769) {
+		$('#mobile_searchh').toggle();
+		$('.search_icon').toggle();
+	}
+	else {
+		$('#desctop_search').toggle();
+		$('.search_icon').toggle();
+	}
 });
 $('.m_menu').click(function () {
 	$('.navbar').toggle('slow');
 });
-/*$('.dropdown').click(function () {
-	if ($(this).hasClass('open')) {
-		$(this).find('.dropdown-menu').hide();
-	}
-	else {
-		$(this).find('.dropdown-menu').show();
-	}
-});
 $('.dropdown-menu li').click(function () {
 	$(this).find('.submenu').toggle();
-});*/
+});
+$(window).resize(function () {
+	if ($(window).width() < 769) {
+		$('.search_form input').removeClass('onTop');
+		$('.nav .dropdown-toggle').attr('data-toggle', 'none');
+		$('.dropdown-toggle').click(function () {
+			if ($(this).parent('#dropdown').hasClass('open')) {
+				$(this).parent('#dropdown').removeClass('open');
+			}
+			else {
+				$(this).parent('#dropdown').addClass('open');
+			}
+		});
+	}
+	else {
+		$('.nav .dropdown-toggle').attr('data-toggle', 'dropdown');
+	}
+});
